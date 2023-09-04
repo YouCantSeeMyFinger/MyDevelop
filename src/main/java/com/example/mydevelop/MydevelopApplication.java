5c9d2786b24d0ea00baf409c40990fac96b64a59
gyoon <xmas6904201@gmail.com>
2023-09-04 22:12:56 +0900

    first commit

---
+++ src/main/java/com/example/mydevelop/MydevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydevelop/MydevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
